b5d4df24e1d7e0ad2aaae3d763c77e70f461198d
jcai8011 <jcai8011@uni.sydney.edu.au>
2023-04-12 11:31:13 +1000

    4 and 8 bit mul

--- MultiPrecision_CNN/MultiPrecision_CNN.srcs/sources_1/new/multiply_4Bit.sv
+++ MultiPrecision_CNN/MultiPrecision_CNN.srcs/sources_1/new/multiply_4Bit.sv
@@ -22,12 +22,13 @@
 // should be noted that the 4 bits in the kernel represent [-1:1]
 module multiply_4Bit #(BitSize = 32, FixedPointPos = 0)
     (
-    input signed [BitSize-1:0] i_data, // 32bit number
-    input signed [3:0] i_prod, // in 2's complement fixed point
-    output logic [BitSize-1:0] o_data
+    input signed [BitSize-1:0]  i_data, // 32bit number
+    input signed [3:0]          i_prod, // in 2's complement fixed point
+    output logic [BitSize-1:0]  o_data
     );
-    wire [2*BitSize-1:0] temp_out;
-    assign temp_out = (i_data*i_prod) >>> FixedPointPos;
-    assign o_data = temp_out[BitSize-1:0];
 
+    wire [2*BitSize-1:0]        temp_out;
+
+    assign temp_out             = (i_data*i_prod) >>> FixedPointPos;
+    assign o_data               = temp_out[BitSize-1:0];
 endmodule
--- MultiPrecision_CNN/MultiPrecision_CNN.srcs/sources_1/new/multiply_8Bit.sv
+++ MultiPrecision_CNN/MultiPrecision_CNN.srcs/sources_1/new/multiply_8Bit.sv
@@ -20,7 +20,15 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module multiply_8Bit(
-    input [0:0] clk
+module multiply_8Bit #(BitSize = 32, FixedPointPos = 0)
+    (
+    input signed [BitSize-1:0]  i_data, // 32bit number
+    input signed [7:0]          i_prod, // in 2's complement fixed point
+    output logic [BitSize-1:0]  o_data
     );
+
+    wire [2*BitSize-1:0]        temp_out;
+
+    assign temp_out             = (i_data*i_prod) >>> FixedPointPos;
+    assign o_data               = temp_out[BitSize-1:0];
 endmodule
