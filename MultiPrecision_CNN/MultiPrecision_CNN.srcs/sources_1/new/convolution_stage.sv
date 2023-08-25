f6957f575d7ec6e15bda68f442b740f15c6fd9b7
jcai8011 <jcai8011@uni.sydney.edu.au>
2023-08-25 12:08:25 +1000

    Update convolution_stage.sv

--- MultiPrecision_CNN/MultiPrecision_CNN.srcs/sources_1/new/convolution_stage.sv
+++ MultiPrecision_CNN/MultiPrecision_CNN.srcs/sources_1/new/convolution_stage.sv
@@ -23,85 +23,85 @@
 // note: N is the size of the kernel - might make it during run-time rather than compilation
 // Some features to add after: - stride steps, increased number of data loaded in at once (parameterised)
 // convolution_stage #(.NumberOfK(), .N(), .BitSize(), .KernelBitSize(), .ImageWidth()) conv_s (.clk(), .res_n(), .in_valid(), .kernel(), .in_data(), .out_ready(), .out_valid(), .out_data());
-module convolution_stage #(NumberOfK = 1, N = 3, BitSize=32, KernelBitSize = 4, ImageWidth = 4)
+
+//implement buffer based on image width (ImageWidth/sqrt(CyclesPerPixel))
+//process each pixel over CPP cycles.
+//ourput is number of kernels  
+
+
+module convolution_stage #(NumberOfK = 1, N = 3, BitSize=32, KernelBitSize = 4, ImageWidth = 4, CyclesPerPixel = 2)
 		(
     		input 							clk,
             input                           res_n,
         	input 							in_valid,     // enable
-
-          	input [NumberOfK*KernelBitSize*(N*N)-1:0] kernel,
-          	input [BitSize-1:0] 			in_data,
-      
+          	input [NumberOfK-1:0][KernelBitSize*(N*N)-1:0] kernel,
+          	input [(N*N)*BitSize-1:0] 			in_data,      
       		output logic 						out_ready,
         	output logic 						out_valid,
-          	output logic [NumberOfK*BitSize-1:0] 			out_data
-      	     
+          	output logic [NumberOfK-1:0][BitSize-1:0] 			out_data // Have to update to number of prtocessing elements  
       	
     );
-  
-  	localparam StreamSize = 	(ImageWidth+N-1)*(N-1)+(N+1); // only works for N = 3
-  
-  	logic [StreamSize-1:0][BitSize-1:0] data_stream_r;
-  	logic [StreamSize-1:0][BitSize-1:0] data_stream_c;
-	
-  	integer 					image_pos_r;
-  	integer						image_pos_c;
-  
-  	logic [N*N-1:0][BitSize-1:0] dot_product_in_c;
+
+	localparam BufferSize = ImageWidth;
+	localparam ProcessingElements = NumberOfK/CyclesPerPixel;
 
 
+	logic [BufferSize-1:0] [(N*N)*BitSize-1:0] buffer_c;
+	logic [BufferSize-1:0] [(N*N)*BitSize-1:0] buffer_r;
+
+	integer buffer_count_c;
+	integer buffer_count_r;
+
+	integer cycle_count_c;
+	integer cycle_count_r;
+
+
+	//make sure output is lined up properly
 	genvar i;
-	genvar j;
 	generate;
-		for (i = 0; i < N; i = i + 1) begin
-			for (j = 0; j < N; j = j + 1) begin
-				assign dot_product_in_c[i][j] = data_stream_r[i * ImageWidth + j];
-			end
+		for (i = 0; i < CyclesPerPixel; i = i + 1) begin
+			 dot_NxN #(.N(N), .BitSize(BitSize), .KernelBitSize(KernelBitSize)) dot_product (.kernel(kernel[i+(cycle_count_c*ProcessingElements)]), .in_data(buffer_c[0]), .out_data(), .sum(out_data[i]));
 		end
 	endgenerate
 
 
   	always_comb
-      begin
-		out_ready = 0;
-        out_valid = 0;
-    	image_pos_c = image_pos_r;
-		data_stream_c = data_stream_r;
-        if(in_valid) begin
-			if (image_pos_c < ImageWidth+2) begin
-				data_stream_c = {data_stream_r[StreamSize-2:0],'0};
-				out_ready = 0;
-			end
-			else if((image_pos_c%(ImageWidth+2) == 0) | (image_pos_c%(ImageWidth+2) == ImageWidth+1)) begin
-				data_stream_c = {data_stream_r[StreamSize-2:0],'0};
-				out_ready = 0;
-			end
-			else begin
-        		data_stream_c = {data_stream_r[StreamSize-2:0], in_data};
-				out_ready = 1;
-			end
-			
-        	image_pos_c = image_pos_c + 1;
-        end
-		else if((image_pos_c < (ImageWidth+2)*(ImageWidth+2)) & (image_pos_c!=0)) begin
-			out_ready = 0;
-			data_stream_c = {data_stream_r[StreamSize-2:0],'0};
-        	image_pos_c = image_pos_c + 1;
+    begin
+	buffer_c 		= buffer_r;
+	buffer_count_c 	= buffer_count_r;
+	cycle_count_c 	= cycle_count_r;
+		if(in_valid)
+		begin
+			// read into the correct count on the register
+			buffer_c[buffer_count_c] 	= in_data;
+			buffer_count_c				= buffer_count_c + 1;
+		end
+
+		// update cycle count and move through kernels, then shift out of buffer reduce buffer count
+		if(cycle_count_c < CyclesPerPixel)
+		begin
+			buffer_c = buffer_c << (CyclesPerPixel*BitSize);
+			cycle_count_c 		= cycle_count_c - CyclesPerPixel;
+						
 		end
     end
-  
-  dot_NxN #(.N(N), .BitSize(BitSize), .KernelBitSize(KernelBitSize)) dot_product (.kernel(kernel), .in_data(dot_product_in_c), .out_data(), .sum(out_data));
-  
-  	always@(posedge clk) begin
+
+
+	always_ff@(posedge clk) begin
     	if(!res_n)
       	begin
-        image_pos_r <= 0;
-		data_stream_r <= 0;
+			buffer_r <= '0;
+			buffer_count_r <= '0;
+			cycle_count_r <= '0;
+
       	end
     	else
       	begin
-        	image_pos_r <= image_pos_c;
-			data_stream_r <= data_stream_c;
+        	buffer_r <= buffer_c;
+			buffer_count_r <= buffer_count_c;
+			cycle_count_r <= cycle_count_c;
         end
   	end
+
+    
 endmodule
