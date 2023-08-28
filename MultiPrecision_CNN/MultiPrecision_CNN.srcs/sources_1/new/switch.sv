86e84f5210aa66f85d50ee60d8c3c04c7b1b69b8
jcai8011 <jcai8011@uni.sydney.edu.au>
2023-08-28 10:54:31 +1000

    Create switch.sv

---
+++ MultiPrecision_CNN/MultiPrecision_CNN.srcs/sources_1/new/switch.sv
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03.04.2023 22:14:21
+// Design Name: 
+// Module Name: convolution_stage
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module swicth #(NumberOfK = 1, CyclesPerPixel = 2)
+		(
+    		input 							clk,
+            input                           res_n,
+        	input 							in_valid,     // enable   
+        	output logic[NumberOfK-1:0]     out_valid,
+      	
+    );
+
+	localparam ProcessingElements 	= NumberOfK/CyclesPerPixel;
+
+    integer count_c;
+    integer count_r;
+
+    always_comb begin
+        count_c     = count_r
+        out_valid   = '0;
+        if(in_valid)
+        begin
+            out_valid[count_c+ProcessingElements-1:count_c] = ~('0);
+            count_c = (count_c + ProcessingElements) % NumberOfK;
+
+        end
+
+
+    end
+
+    always_ff@(posedge clk) begin
+        if(!res_n) begin
+            count_r     <= '0;
+            out_valid   <='0;
+        end
+        else
+        begin
+            count_r     <= count_c;
+        end
+    end
+
+endmodule
