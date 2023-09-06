961debbca7ab0028ef7d6454e173e8e229dbeb26
EdisonLam-USYD <80366397+EdisonLam-USYD@users.noreply.github.com>
2023-09-06 13:46:40 +1000

    working versions

--- MultiPrecision_CNN/MultiPrecision_CNN.srcs/sim_1/new/TB_flattening_pe.sv
+++ MultiPrecision_CNN/MultiPrecision_CNN.srcs/sim_1/new/TB_flattening_pe.sv
@@ -30,8 +30,6 @@ module TB_flattening_pe;
     logic in_valid;
     logic [BitSize-1:0] in_data;
 
-    logic out_ready;
-    logic out_valid;
     logic [N-1:0][N-1:0][BitSize-1:0] out_data;
     logic out_done;
 
@@ -39,17 +37,28 @@ module TB_flattening_pe;
 
 
     
-    flattening_pe #(.BitSize(BitSize), .ImageSize(ImageWidth*ImageWidth), .Delay(0)) flat_pe 
-        (.clk(clk), .res_n(res_n), .in_valid(in_valid), .in_data(in_data), .out_valid(out_valid), .out_data(out_data), .out_done(out_done));
+    flattening_pe #(.BitSize(BitSize), .ImageSize(ImageWidth*ImageWidth), .Delay(3)) flat_pe 
+        (.clk(clk), .res_n(res_n), .in_valid(in_valid), .in_data(in_data), .out_data(out_data), .out_done(out_done));
 
 //    assign {>>{out_data_vis}} = out_data;
     
     initial
     begin
 
-        test_image = {1,2,3,4,5,6,7,8,9};
+        test_image = {
+            BitSize'(1),
+            BitSize'(2),
+            BitSize'(3),
+            BitSize'(4),
+            BitSize'(5),
+            BitSize'(6),
+            BitSize'(7),
+            BitSize'(8),
+            BitSize'(9)
+        };
         
         res_n = 0;
+        in_data = '0;
         clk = 0;
         #5
         clk = 1;
@@ -61,9 +70,9 @@ module TB_flattening_pe;
         // $monitor("@ %0t:\tbuffer_r = %p\n\t\t\tout = %p, out_valid = %b, pos_c = %d\n\t\t\trow_c = %d, out_ready = %b", 
         //     $time, conv_b.data_stream_c, out_data, out_valid, conv_b.image_pos_c, conv_b.image_row_c, conv_b.out_ready);
 
-        for (int counter = 1; counter <= ImageWidth*ImageWidth; counter = counter + 1) begin
+        for (int counter = 0; counter < ImageWidth*ImageWidth; counter = counter + 1) begin
             #5
-            in_data = test_image[ImageWidth*ImageWidth - counter];
+            in_data = test_image[ImageWidth*ImageWidth - 1 - counter];
 //            in_done = (ImageWidth*ImageWidth - counter == 0) ? 1 : 0;
             in_valid = 1;
             clk = 1;
--- MultiPrecision_CNN/MultiPrecision_CNN.srcs/sources_1/new/flattening_pe.sv
+++ MultiPrecision_CNN/MultiPrecision_CNN.srcs/sources_1/new/flattening_pe.sv
@@ -1,7 +1,7 @@
 // takes the in_data on the same cycle as the res_n (assuming in_valid is high)
 
 // flattening_pe #(.BitSize(), .ImageSize(), .Delay()) flat_pe 
-//     (.clk(), .res_n(), .in_valid(), .in_data(), .out_valid(), .out_data(), .out_done());
+//     (.clk(), .res_n(), .in_valid(), .in_data(), .out_data(), .out_done());
 module flattening_pe #(BitSize = 2, ImageSize = 9, Delay = 0) 
     (
         input clk,
@@ -9,30 +9,28 @@ module flattening_pe #(BitSize = 2, ImageSize = 9, Delay = 0)
         input in_valid,
         input [BitSize-1:0] in_data,
 
-        output logic                                        out_valid,
         // output logic                                        out_ready, // does not seem necessary
-        output logic [(ImageSize)*BitSize-1:0] out_data,
+        output logic [ImageSize-1:0][BitSize-1:0] out_data,
         output logic out_done       // not sure if needed
     );
 
     logic [$clog2(ImageSize + Delay)-1:0] counter_r;
     logic [$clog2(ImageSize + Delay)-1:0] counter_c;
 
-    logic [ImageSize-1:0][BitSize-1:0] out_data_c;
-    logic in_data_c;
+    // logic [ImageSize-1:0][BitSize-1:0] out_data_c;
+    logic [BitSize-1:0] in_data_c;
     
-    assign out_data = out_data_c;
 
     always_comb begin
         counter_c = counter_r;
-        out_data_c = '0;
+        out_data = '0;
         out_done = 0;
         if (in_valid)
         begin
-            if (counter_c >= Delay) out_data_c[ImageSize-1-counter_c] =  in_data_c;
+            if (counter_c >= Delay) out_data[ImageSize-1-counter_c+Delay] =  in_data_c;
             // counter_c = (ImageSize - 1 != counter_r) ? counter_r + 1 : 0;
             counter_c = counter_r + 1;
-            out_done = (counter_c >= ImageSize + Delay) ? 1 : 0;
+            out_done = (counter_c >= ImageSize + Delay + 1) ? 1 : 0;
         end
     end
 
@@ -55,23 +53,20 @@ module flattening_pe #(BitSize = 2, ImageSize = 9, Delay = 0)
         begin
             for (i = 0; i <= Delay; i = i + 1)
             begin : s_del
-                if (i < Delay)
-                begin
-                    logic [BitSize-1:0] buffer;
-                end
+                logic [BitSize-1:0] buffer;
 
                 if (i == 0)
                 begin
                     always_ff @(posedge clk) 
                     begin
-                        if (!res_n) s_del[i].buffer <= '0;
+                        if (!res_n) s_del[0].buffer <= '0;
                         else
                         begin
-                            if (in_valid) s_del[i].buffer <= in_data;
+                            if (in_valid) s_del[0].buffer <= in_data;
                         end
                     end
                 end
-                else if (i < Delay)
+                else
                 begin
                     always_ff @(posedge clk) 
                     begin
@@ -82,11 +77,8 @@ module flattening_pe #(BitSize = 2, ImageSize = 9, Delay = 0)
                         end
                     end
                 end
-                else 
-                begin
-                    assign in_data_c = s_del[i-1].buffer;
-                end
-            end
+            end 
+            assign in_data_c = s_del[Delay].buffer; // may need to make it Delay - 1 depending on TB
         end
     endgenerate
 
