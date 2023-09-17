c092d73c085b8eac1eeac73a5005bac45452cecc
EdisonLam-USYD <80366397+EdisonLam-USYD@users.noreply.github.com>
2023-09-18 00:55:53 +1000

    confirmed the loading in of weights work

--- MultiPrecision_CNN/MultiPrecision_CNN.srcs/sim_1/new/TB_dnn.sv
+++ MultiPrecision_CNN/MultiPrecision_CNN.srcs/sim_1/new/TB_dnn.sv
@@ -21,34 +21,104 @@
 
 module TB_dnn;
     localparam BitSize          = 4;
-    // localparam ImageSize        = 2; 
-    // localparam NumOfImages      = 4;
+    localparam ImageSize        = 4; 
+    localparam NumOfImages      = 2;
     // localparam NumOfPEPerInput  = 1;
-    // localparam NumOfInputs      = 2;
+     localparam NumIn      = 1;
     // localparam CyclesPerPixel   = 2;
+    localparam MaxNumNerves = 3;
     localparam M_W_BitSize      = 4;
-    localparam bit [4:0] a [4:0] = '{2, 4, 6, 8, 10};
+    localparam integer LNN [1:0] = '{2, 3};
 
     logic clk;
     logic res_n;
     logic [NumOfImages-1:0] in_valid;
-    logic [NumOfInputs-1:0][BitSize-1:0] in_data;
+    logic [NumIn-1:0][BitSize-1:0] in_data;
+    // logic [NumOfImages-1:0][ImageSize-1:0][BitSize-1:0] inputs;
+    logic [ImageSize-1:0][BitSize-1:0] in_a;
+    logic [ImageSize-1:0][BitSize-1:0] in_b;
+
+    logic [MaxNumNerves-1:0][M_W_BitSize-1:0] in_weight;
 
     logic out_ready;
     logic out_valid;
+    logic out_done;
+    logic [2:0][BitSize-1:0] out_data;
 
     dnn_top #(
-        .BitSize(BitSize), .M_W_BitSize(M_W_BitSize), .NumIn(1), .MaxNumNerves(3), .NumOfImages(2),
-        .CyclesPerPixel(1), .ImageSize(4), .NumLayers(2), .LWB(`{4, 2}), .LNN(`{3, 3})
+        .BitSize(BitSize), .M_W_BitSize(M_W_BitSize), .NumIn(1), .MaxNumNerves(3), .NumOfImages(NumOfImages),
+        .CyclesPerPixel(1), .ImageSize(ImageSize), .NumLayers(2), .LWB('{4, 2}), .LNN('{2, 3}) 
     ) dnn_inst (
-        .clk(clk), .res_n(res_n), .in_valid(in_valid), .in_data(in_data), .in_weights(), 
-        .out_ready(), .out_data(), .out_valid(), .out_done()
+        .clk(clk), .res_n(res_n), .in_valid(in_valid), .in_data(in_data), .in_weights(in_weight), 
+        .out_ready(out_ready), .out_data(out_data), .out_valid(out_valid), .out_done(out_done)
     );
 
-    // testing array parameters:
-    initial begin
-        for (int i = 0; i < 5; i = i + 1) begin
-            $display("i = %d: a[i] = %d", i, a[i]);
+    // testing array parameters: (works)
+    // initial begin
+    //     for (int i = 0; i < 5; i = i + 1) begin
+    //         $display("i = %d: a[i] = %d", i, a[i]);
+    //     end
+    // end
+
+    logic [M_W_BitSize-1:0] a;
+    logic [M_W_BitSize-1:0] b;
+    logic [M_W_BitSize-1:0] c;
+    logic [M_W_BitSize-1:0] d;
+    logic [ImageSize-1:0][LNN[1]-1:0][M_W_BitSize-1:0] weights0;
+    logic [LNN[1]-1:0][LNN[0]-1:0][M_W_BitSize-1:0] weights1;
+
+    initial begin 
+        // monitor to check the loading in of weights
+       $monitor("@%0t: \n\tres_n = %b, in_weight = %p, weight_en = %p \n\tout_ready = %p", $time, res_n, in_weight, dnn_inst.weight_en, out_ready); 
+        in_a = {BitSize'(4), BitSize'(4), BitSize'(4), BitSize'(4)};
+        in_b = {BitSize'(3), BitSize'(3), BitSize'(3), BitSize'(3)};
+
+        a = 4'b0111;
+        b = 4'b0010;
+        c = 4'b0001;
+        d = 4'b0000;
+        weights0 = {{c, d},
+                    {a, c},
+                    {d, c},
+                    {a, b}};
+
+        weights1 = {{c, d, c},
+                    {a, b, c}};
+
+        res_n = 0;
+        in_valid = 0;
+        clk = 0;
+        // starting the weight loading process on next posedge clock
+
+        for (int i = 0; i < ImageSize; i = i + 1) begin
+            #10
+            in_weight = {weights0[i], M_W_BitSize'(0)}; // in_weight = {weights0[ImageSize-1-i], M_W_BitSize'(0)};
+            clk = 1;
+            #10
+            res_n = 1;
+            clk = 0;
         end
+
+        for (int i = 0; i < LNN[1]; i = i + 1) begin
+            #10
+            in_weight = {weights1[i]};                     // in_weight = {weights1[LNN[1]-1-i]};
+            clk = 1;
+            #10
+            clk = 0;
+        end
+
+        #10
+        in_weight = '0;
+        in_valid = 0;
+        clk = 1;
+        #10
+        clk = 0;
+        #10
+        in_weight = '0;
+        in_valid = 0;
+        clk = 1;
+        #10
+        clk = 0;
+
     end
 endmodule
